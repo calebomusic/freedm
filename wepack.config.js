c72559c8bbb1efe56a348cf336244601820ff103
calebomusic <calebomusic@gmail.com>
2016-11-20 20:52:43 -0800

    update webpack.config

--- wepack.config.js
+++ wepack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  // entry: Change me,
+  entry: 'frontend/freedm.jsx',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
