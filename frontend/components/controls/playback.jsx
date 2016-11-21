3beb34aa0f51e66eaa5431ec9b63618077bbf183
calebomusic <calebomusic@gmail.com>
2016-11-21 12:14:00 -0800

    Add playback

--- frontend/components/controls/playback.jsx
+++ frontend/components/controls/playback.jsx
@@ -5,6 +5,31 @@ import React from 'react';
 class Playback extends React.Component {
   constructor(props) {
     super(props);
+
+    this.playback = window.setInterval(this.step, this.props.bpm / 600)
+  }
+
+  start() {
+    if (this.playback) {
+      this.changeColumn()
+    }
+  }
+
+  stop() {
+    clearInterval(this.playback);
+    this.props.stopPlayback;
+  }
+
+  step() {
+    if (this.props.playback) {
+      this.start();
+    }
+  }
+
+  changeColumn() {
+    window.clearInterval(this.playback)
+    this.props.updateColumn()
+    this.playback = window.setInterval(this.changeColumn, this.props.bpm / 600)
   }
 
   render() {
