bb4db6cd2e8e2e15e9b96647efb4992f09fd389d
Eric Kwok <ericc.kwok@gmail.com>
2016-11-20 22:25:05 -0800

    Edit actions

--- frontend/actions/selected_sounds_actions.js
+++ frontend/actions/selected_sounds_actions.js
@@ -1,53 +1,18 @@
-export const REQUEST_SELECTED_SOUNDS = "REQUEST_SELECTED_SOUNDS";
-export const REQUEST_SELECTED_SOUNDS = "REQUEST_SELECTED_SOUNDS";
-export const RECEIVE_SELECTED_SOUNDS = "RECEIVE_SELECTED_SOUNDS";
-export const RECEIVE_SELECTED_SOUNDS = "RECEIVE_SELECTED_SOUNDS";
+export const ADD_SELECTED_SOUND = "ADD_SELECTED_SOUND";
+export const REMOVE_SELECTED_SOUND = "REMOVE_SELECTED_SOUND";
 export const REMOVE_SELECTED_SOUNDS = "REMOVE_SELECTED_SOUNDS";
-export const CREATE_SELECTED_SOUNDS = "CREATE_SELECTED_SOUNDS";
-export const UPDATE_SELECTED_SOUNDS = "UPDATE_SELECTED_SOUNDS";
-export const DESTROY_SELECTED_SOUNDS = "DESTROY_SELECTED_SOUNDS";
-export const SELECTED_SOUNDS_ERROR = "SELECTED_SOUNDS_ERROR";
 
-export const requestSelectedSounds = () => ({
-  type: REQUEST_SELECTED_SOUNDS
+export const addSelectedSound = selectedSound => ({
+  type: ADD_SELECTED_SOUND,
+  selectedSound
 });
 
-export const requestSelectedSounds = id => ({
-  type: REQUEST_SELECTED_SOUNDS,
-  id
+export const removeSelectedSound = selectedSound => ({
+  type: REMOVE_SELECTED_SOUND,
+  selectedSound
 });
 
-export const receiveSelectedSounds = selected_sounds => ({
-  type: RECEIVE_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const receiveSelectedSounds = selected_sounds => ({
-  type: RECEIVE_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const removeSelectedSounds = selected_sounds => ({
+export const removeSelectedSounds = selectedSounds => ({
   type: REMOVE_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const createSelectedSounds = selected_sounds => ({
-  type: CREATE_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const updateSelectedSounds = selected_sounds => ({
-  type: UPDATE_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const destroySelectedSounds = selected_sounds => ({
-  type: DESTROY_SELECTED_SOUNDS,
-  selected_sounds
-});
-
-export const selected_soundsError = error => ({
-  type: SELECTED_SOUNDS_ERROR,
-  error
+  selectedSounds
 });
--- frontend/actions/tab_actions.js
+++ frontend/actions/tab_actions.js
@@ -1,53 +1,6 @@
-export const REQUEST_TABS = "REQUEST_TABS";
-export const REQUEST_TAB = "REQUEST_TAB";
-export const RECEIVE_TABS = "RECEIVE_TABS";
-export const RECEIVE_TAB = "RECEIVE_TAB";
-export const REMOVE_TAB = "REMOVE_TAB";
-export const CREATE_TAB = "CREATE_TAB";
 export const UPDATE_TAB = "UPDATE_TAB";
-export const DESTROY_TAB = "DESTROY_TAB";
-export const TAB_ERROR = "TAB_ERROR";
-
-export const requestTabs = () => ({
-  type: REQUEST_TABS
-});
-
-export const requestTab = id => ({
-  type: REQUEST_TAB,
-  id
-});
-
-export const receiveTabs = tabs => ({
-  type: RECEIVE_TABS,
-  tabs
-});
-
-export const receiveTab = tab => ({
-  type: RECEIVE_TAB,
-  tab
-});
-
-export const removeTab = tab => ({
-  type: REMOVE_TAB,
-  tab
-});
-
-export const createTab = tab => ({
-  type: CREATE_TAB,
-  tab
-});
 
 export const updateTab = tab => ({
   type: UPDATE_TAB,
   tab
-});
-
-export const destroyTab = tab => ({
-  type: DESTROY_TAB,
-  tab
-});
-
-export const tabError = error => ({
-  type: TAB_ERROR,
-  error
 });
