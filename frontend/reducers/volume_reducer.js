8ef779d70a56381af642f6d49dcf9c4d9107d124
calebomusic <calebomusic@gmail.com>
2016-11-23 14:59:24 -0800

    Add master volume controls with howler

--- frontend/reducers/volume_reducer.js
+++ frontend/reducers/volume_reducer.js
@@ -1,7 +1,7 @@
 import { UPDATE_VOLUME } from '../actions/volume_actions';
 import merge from 'lodash/merge';
 
-const VolumeReducer = (state = {}, action) => {
+const VolumeReducer = (state = 0.7, action) => {
   switch(action.type){
     case UPDATE_VOLUME:
       return action.volume;
