85bb310a2ac80036704fc0d507a93593d23b4d01
calebomusic <calebomusic@gmail.com>
2016-11-22 17:34:55 -0800

    Add lead synth wavs, test sound

---
+++ frontend/util/lead_synth_1.js
@@ -0,0 +1,11 @@
+import { Howl } from 'howler';
+
+export const A = new Howl({
+  src: ['samples/lead-synths/1/1 A.wav']
+ }
+)
+
+export const Bb = new Howl({
+  src: ['samples/lead-synths/1/1 A.wav']
+ }
+)
