61258731e90ebc79d5ecc4efe8d11265e40e2f87
Eric Kwok <ericc.kwok@gmail.com>
2016-11-28 18:31:20 -0800

    Add bar visualizer

--- frontend/components/visualizer/visualizer.jsx
+++ frontend/components/visualizer/visualizer.jsx
@@ -7,19 +7,29 @@ class Visualizer extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {visualizer: "bars"};
+
     this.setup = this.setup.bind(this);
+    this.drawLine = this.drawLine.bind(this);
     this.drawBars = this.drawBars.bind(this);
   }
 
   componentWillMount() {
-    this.loading();
-
-    setTimeout(this.setup, 1000);
-    setTimeout(this.drawBars, 1000);
+    // setTimeout(this.setupLine, 100);
+    // setTimeout(this.drawLine, 100);
+    setTimeout(this.setup, 100);
+    setTimeout(this.drawBars, 100);
   }
 
-  loading() {
-    // INSERT COOL LOADER
+  toggleVisualizer(type) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({visualizer: type});
+    };
+  }
+
+  isActive (type) {
+    return ((type === this.state.visualizer) ? "selected" : "hidden" );
   }
 
   setup() {
@@ -31,28 +41,32 @@ class Visualizer extends React.Component {
 
     this.canvasCtx = canvas.getContext("2d");
 
+    // // this.bufferLength = this.analyser.frequencyBinCount;
+    // this.bufferLength = 800;
+    // this.dataArray = new Uint8Array(this.bufferLength);
+    // this.analyser.getByteTimeDomainData(this.dataArray);
+
+    this.analyser.fftSize = 256;
     this.bufferLength = this.analyser.frequencyBinCount;
     this.dataArray = new Uint8Array(this.bufferLength);
-    this.analyser.getByteTimeDomainData(this.dataArray);
   }
 
-  drawBars() {
-    this.canvasCtx.clearRect(0, 0, 500, 110);
-    const drawVisual = requestAnimationFrame(this.drawBars);
+  drawLine() {
+    const drawVisual = requestAnimationFrame(this.drawLine);
     this.analyser.getByteTimeDomainData(this.dataArray);
     this.canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    this.canvasCtx.fillRect(0, 0, 500, 110);
+    this.canvasCtx.fillRect(0, 0, 450, 150);
     this.canvasCtx.lineWidth = 2;
     this.canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
     this.canvasCtx.beginPath();
 
-    const sliceWidth = 400 * 1.0 / this.bufferLength;
+    const sliceWidth = 450 * 1.0 / this.bufferLength;
     let x = 0;
 
     for (var i = 0; i < this.bufferLength; i++) {
-
       const v = this.dataArray[i] / 128.0;
-      var y = v * 400 / 2;
+      var y = v * 450 / 2;
+
       if (i === 0) {
         this.canvasCtx.moveTo(x, y);
       } else {
@@ -62,19 +76,47 @@ class Visualizer extends React.Component {
       x += sliceWidth;
     }
 
-    this.canvasCtx.lineTo(500, 110/2);
+    this.canvasCtx.lineTo(450, 110/2);
     this.canvasCtx.stroke();
   }
 
-  render() {
-    if (this.canvas) {
-      this.setup();
-      this.drawBars();
+  drawBars() {
+    this.canvasCtx.clearRect(0, 0, 450, 150);
+    const drawVisual = requestAnimationFrame(this.drawBars);
+    this.analyser.getByteFrequencyData(this.dataArray);
+
+    this.canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    this.canvasCtx.fillRect(0, 0, 450, 150);
+
+    var barWidth = (450 / this.bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    for(var i = 0; i < this.bufferLength; i++) {
+      barHeight = this.dataArray[i];
+
+      this.canvasCtx.fillStyle = `rgb(${barHeight/2+30},${barHeight/2+55},${barHeight/2+30})`;
+      this.canvasCtx.fillRect(x,110-barHeight/2,barWidth,barHeight/2);
+
+      x += barWidth + 1;
     }
+  }
 
+  render() {
     return(
-      <div>
-
+      <div className="vis-toggle-container">
+        <div className={`btn-bars ${this.isActive("bars")}`}
+             onClick={this.toggleVisualizer("line")}>
+          BARS
+        </div>
+        <div className={`btn-line ${this.isActive("line")}`}
+             onClick={this.toggleVisualizer("none")}>
+          LINE
+        </div>
+        <div className={`btn-none ${this.isActive("none")}`}
+             onClick={this.toggleVisualizer("bars")}>
+          NONE
+        </div>
       </div>
     );
   }
